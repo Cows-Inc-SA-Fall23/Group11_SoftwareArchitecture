c007cc442e1e3c6c3be5f932ece57f94c63106f2
Ignatios Mantanis <126020111+ignatios161@users.noreply.github.com>
2023-11-25 16:34:33 +0100

    corrections

--- src/inboundSystemContainer/inboundSystem/src/test/java/test/madasi/inboundSystem/KafkaIntegrationTest.java
+++ src/inboundSystemContainer/inboundSystem/src/test/java/test/madasi/inboundSystem/KafkaIntegrationTest.java
@@ -6,10 +6,9 @@ import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import madasi.inboundSystem.labSystemApplication;
+import madasi.inboundSystem.inboundSystemApplication;
 import madasi.inboundSystem.service.ConsumerService;
 import madasi.inboundSystem.service.ProducerService;
 
