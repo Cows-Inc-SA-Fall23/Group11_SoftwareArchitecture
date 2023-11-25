44ef14a83908ce69f43293bf76e486ade797e1e1
Ignatios Mantanis <126020111+ignatios161@users.noreply.github.com>
2023-11-25 15:13:04 +0100

    .

--- src/inboundSystemContainer/inboundSystem/src/main/java/madasi/labSystem/ServletInitializer.java
+++ src/inboundSystemContainer/inboundSystem/src/main/java/madasi/labSystem/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(labSystemApplication.class);
+		return application.sources(inboundSystemApplication.class);
 	}
 
 }
